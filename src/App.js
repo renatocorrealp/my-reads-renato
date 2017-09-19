40764645a38d0407822ccd613f13a2ae3389aef5
Renato Oliveira Correa <t1501rnt@cielo.com.br>
2017-09-18 23:41:08 -0300

    fix: Limpeza de código
      - Remoção de códigos comentados
      - Identação de código
      - Removendo arquivos não mais utilizados
      - Otimização de CSS
      - Reorganizando componentes.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
-import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI';
 import './App.css';
 import SearchBook from './components/SearchBook';
-import ListBooks from './components/ListBooks';
-import {Link} from 'react-router-dom';
+import BooksShelf from './components/Shelf';
 import {Route}from 'react-router-dom';
 class BooksApp extends Component {
   constructor(props){
@@ -15,6 +14,7 @@ class BooksApp extends Component {
     this.updateBookShelf = this.updateBookShelf.bind(this);
   }
 
+  // Updates book shelf
   updateBookShelf = (updateBook, shelf) => {
     this.setState(prev => {
       const {books} = prev;
@@ -30,56 +30,31 @@ class BooksApp extends Component {
     BooksAPI.update(updateBook, shelf);
   }
 
-  /*filterBooksByShelf = function(filter){
-    return this.state.books.filter((book) => book.shelf === filter);
-  }*/
-
-  filterBooksByShelf = filter => this.state.books.filter((book) => book.shelf === filter);
-
+  // Calls API to get books in shelf
   componentDidMount = () => BooksAPI.getAll().then((books) => this.setState({books}));
 
   render() {
-    const currentlyReading = this.filterBooksByShelf('currentlyReading');
-    const wantToRead = this.filterBooksByShelf('wantToRead');
-    const read = this.filterBooksByShelf('read');
+    const {books} = this.state;
 
     return (
       <div className="app">
-        <Route path="/search" render={() => (
-          <SearchBook booksShelved={this.state.books} onUpdateBookShelf={this.updateBookShelf}/>
-        )} />
-        <Route exact path="/" render={() => (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ListBooks books={currentlyReading} onUpdateBookShelf={this.updateBookShelf}/>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ListBooks books={wantToRead} onUpdateBookShelf={this.updateBookShelf}/>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ListBooks books={read} onUpdateBookShelf={this.updateBookShelf}/>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="open-search">
-              <Link to="/search">Add a book</Link>
-            </div>
-          </div>
-        )}/>
+        <Route
+          path="/search"
+          render={() => (
+            <SearchBook
+              booksShelved={books}
+              onUpdateBookShelf={this.updateBookShelf}/>
+          )}
+        />
+      <Route
+        exact
+        path="/"
+        render={() => (
+          <BooksShelf
+            updateBookShelf={this.updateBookShelf}
+            books={books}/>
+        )}
+        />
       </div>
     )
   }
--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -12,6 +12,7 @@ class SearchBook extends Component{
     this.timeout = null;
   }
 
+  // Search book when user enter something in search input
   searchBook = query => {
     // Clear timeout if the user still typing
     clearTimeout(this.timeout);
@@ -19,6 +20,7 @@ class SearchBook extends Component{
     // Wait for user stop typing
     this.timeout = setTimeout(() => {
       if(!!query){
+        // Call API of search of books
         BooksAPI.search(query, 10).then((booksResult) => {
           this.loadBooksShelves(booksResult);
           this.setState({booksResult});
@@ -29,6 +31,7 @@ class SearchBook extends Component{
     }, 500);
   }
 
+  // Set the book's shelf status based the 'booksShelved' props
   loadBooksShelves = booksResult => {
     const {booksShelved} = this.props;
 
@@ -50,16 +53,7 @@ class SearchBook extends Component{
         <div className="search-books-bar">
           <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
             <input type="text" onChange={(event) => this.searchBook(event.target.value)} placeholder="Search by title or author"/>
-
           </div>
         </div>
         <div className="search-books-results">
